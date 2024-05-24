d7c50c2f4876d42d8c595f642b34ab08f6d820a7
JIHONG KIM <micael17@naver.com>
2024-05-25 00:58:27 +0900

    댓글 스타일 정의

--- components/board/viewer.tsx
+++ components/board/viewer.tsx
@@ -50,6 +50,33 @@ export default function Viewer(props: Props) {
         {/* <ReactQuill readOnly modules={modules} theme="snow" value={props.writing.content} /> */}
       </div>
       <hr />
+      <div className={style.comment_container}>
+        <div className={style.comment_cnt}>2 comments</div>
+        <div className={`${style.comment}`}>
+          <div className={style.comment_header}>
+            <div className={style.comment_info}>by: 사용자1, 작성일: 2024-05-24</div>
+          </div>
+          <div className={style.comment_content}>
+            <p>이 댓글은 댓글 내용입니다.</p>
+          </div>
+        </div>
+        <div className={`${style.comment} ${style.nested_comment}`}>
+          <div className={style.comment_header}>
+            <div className={style.comment_info}>by: 사용자2, 작성일: 2024-05-25</div>
+          </div>
+          <div className={style.comment_content}>
+            <p>댓글의 댓글 내용은 여기에 있습니다.</p>
+          </div>
+        </div>
+        <div className={`${style.comment} ${style.nested_comment}`}>
+          <div className={style.comment_header}>
+            <div className={style.comment_info}>by: 사용자2, 작성일: 2024-05-25</div>
+          </div>
+          <div className={style.comment_content}>
+            <p>더 깊은 단계의 댓글 내용은 여기에 있습니다.</p>
+          </div>
+        </div>
+      </div>
       <div className={style.buttons}>
         <Link as={NextLink} href="/board">
           <Button className={style.button}>게시판으로 가기</Button>
